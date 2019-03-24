cc83ca9b06d611b9a5df2c2d012602b2dc6a8394
Gullaprathapreddy <prathapreddy2662@gmail.com>
2019-03-24 14:46:56 +0530

    boot application created

---
+++ Spring-Boot-EmployeeAplplication/src/main/java/com/pt/sb/SpringBootEmployeeAplplication/SpringBootEmployeeAplplicationApplication.java
@@ -0,0 +1,13 @@
+package com.pt.sb.SpringBootEmployeeAplplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmployeeAplplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmployeeAplplicationApplication.class, args);
+	}
+
+}
